a86cdb4bcdf2bfd34425c28ee2f3ee77235011cd
Zer0detail <zer0detail@protonmail.com>
2021-09-07 13:36:36 +1000

    add rust gitignore

--- Intro/csv_processing_example/target/rls/debug/deps/csv_processing_example-7153c8e8988f6e03.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Aaron\Desktop\RustStuff\rust-in-action\Intro\csv_processing_example\target\rls\debug\deps\csv_processing_example-7153c8e8988f6e03.rmeta: src\main.rs
-
-c:\Users\Aaron\Desktop\RustStuff\rust-in-action\Intro\csv_processing_example\target\rls\debug\deps\csv_processing_example-7153c8e8988f6e03.d: src\main.rs
-
-src\main.rs:
--- Intro/csv_processing_example/target/rls/debug/deps/csv_processing_example-e5f38a6db1fd369a.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Aaron\Desktop\RustStuff\rust-in-action\Intro\csv_processing_example\target\rls\debug\deps\csv_processing_example-e5f38a6db1fd369a.rmeta: src\main.rs
-
-c:\Users\Aaron\Desktop\RustStuff\rust-in-action\Intro\csv_processing_example\target\rls\debug\deps\csv_processing_example-e5f38a6db1fd369a.d: src\main.rs
-
-src\main.rs:
