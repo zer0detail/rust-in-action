ed899ef22d05f90ecef347f7f2f3df346ff7e307
Zer0detail <zer0detail@protonmail.com>
2021-09-13 09:19:53 +1000

    next progress commit

--- Part 1/Language Foundations/matching/src/main.rs
+++ Part 1/Language Foundations/matching/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let needle = 42;
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    for item in &haystack {
+        let result = match item {
+            42 | 132 => "Hit!",
+            _ => "miss",
+        };
+    if result == "Hit!" {
+        println!("{}: {}", item, result);
+    }
+    }
 }
